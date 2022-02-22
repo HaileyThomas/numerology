d2447c4e6e63f077b9211143e6679bbdde5769df
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-22 14:20:20 -0700

    finish basic psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+// DOM elements
 var formEl = document.querySelector("#main-form");
 var numInput = document.querySelector("#number-list");
 var maxInput = document.querySelector("#max-input");
@@ -7,10 +8,18 @@ var formSubmitHandler = function (event) {
   event.preventDefault();
 
   // check to see if results container already has results and clear
-  // get value for number one input
-  // get value for number two input
-  // create an array made up of number one input + 9 continued until it reaches number two input
-  // add together every digit in array
+  // get value for number input and create variable for tableNumber
+  // get value for max input and create variable for maxNumber
+  // create array of numbers that starts with tableNumber plus 9,
+  // - gets the sum, adds that to the next index of the array, then continues
+  // - until the sum reaches the maxNumber
+  // take each number from array and print the sum of each digit
+  // - until it reaches the original table number
+  // - ie: tableNumber is 2, second index would be 11, it takes 11 and
+  // - breaks it down to 1 + 1 = 2, stops because we are at tableNumber
+  // - next index is 20, breaks down to 2 + 0 and stops because it's 2
+  // - fourth index is 29, breaks down to 9 + 2 = 11, continues to
+  // - 1 + 1 = 2 then stops
 };
 
 formEl.addEventListener("submit", formSubmitHandler);
