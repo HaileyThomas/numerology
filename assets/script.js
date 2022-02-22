fc267fe68fa281c6996742878fb58e6a355fb6b8
Hailey Thomas <haileyraebeauty@gmail.com>
2022-02-22 13:07:23 -0700

    start psuedocode

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,16 @@
 var formEl = document.querySelector("#main-form");
+var numOneInput = document.querySelector("#number-list");
+var numTwoInput = document.querySelector("#number-input");
+var resultsContainer = document.querySelector("#table-container");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
+
+  // check to see if results container already has results and clear
+  // get value for number one input
+  // get value for number two input
+  // create an array made up of number one input + 9 continued until it reaches number two input
+  // add together every digit in array
 };
 
 formEl.addEventListener("submit", formSubmitHandler);
